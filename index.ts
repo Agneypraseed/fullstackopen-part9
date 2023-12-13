d0b2811173f02e7996527704227d9a9922a4bea2
Agney Praseed <agneysince2000@gmail.com>
2023-12-13 22:32:10 +0530

    9.7 WebExercises

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import calculateBmi from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
+import { isNotNumber } from "./utils";
 import express = require("express");
 const app = express();
+
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
     res.send("Hello Full Stack!");
@@ -16,6 +20,23 @@ app.get("/bmi/", (req, res) => {
 
     const bmi = calculateBmi(height, weight);
     return res.json({ height, weight, bmi });
+});
+
+app.post("/exercises/", (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { daily_exercises, target } = req.body;
+    if (target == null || !daily_exercises)
+        res.json({
+            error: "parameters missing",
+        });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const allValuesAreNumbers: boolean = daily_exercises.slice(2).every((value: any) => !isNotNumber(value));
+    if (isNotNumber(target) || !allValuesAreNumbers) {
+        res.json({
+            error: "malformatted parameters",
+        });
+    }
+    res.json(calculateExercises(daily_exercises, Number(target)));
 });
 
 const PORT = 3003;
